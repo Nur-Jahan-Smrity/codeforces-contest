394df96c3feb454effe6dcc22c45c5d01b37523f
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-03-28 13:03:03 +0600

    Create A. Plus or Minus(859).cpp

---
+++ A. Plus or Minus(859).cpp
@@ -0,0 +1,40 @@
+//CF_A_
+#include<bits/stdc++.h>
+#define all(qz) qz.begin(),qz.end()
+#define rall(qz) qz.rbegin(),qz.rend()
+#define pi (3.141592653589)
+// reverse(s.begin(), s.end());
+using namespace std;
+typedef long long ll;
+//int j=0;
+void solve()
+{
+    long long n,i,j,count=0,a,b,c;
+    cin>>a>>b>>c;
+    if(abs(a-b)==abs(c))
+        cout<<"-"<<endl;
+    else
+           cout<<"+"<<endl;
+}
+void test()
+{
+    long long t;
+    cin>>t;
+
+    while(t--)
+    {
+        //j++;
+        solve();
+    }
+
+}
+
+int main()
+{
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    test();
+    //solve();
+}
+/*
+
+*/
