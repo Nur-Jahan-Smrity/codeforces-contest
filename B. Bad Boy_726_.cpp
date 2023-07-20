56919accd8295ee62c9cb1df4819352b84f18090
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-07-20 20:06:54 +0600

    Create B. Bad Boy_726_.cpp

---
+++ B. Bad Boy_726_.cpp
@@ -0,0 +1,41 @@
+//B. Bad Boy_726_.cpp
+#include<bits/stdc++.h>
+
+  // reverse(s.begin(), s.end());
+using namespace std;
+typedef long long ll;
+//int j=0;
+
+void solve()
+{
+
+   long long n,m,p,q;
+   cin>>n>>m>>p>>q;
+   cout<<"1 1 "<<n<<" "<<m<<endl;
+}
+void test()
+{
+    long long t;
+    cin>>t;
+
+    while(t--)
+    {
+        //j++;
+        solve();
+    }
+
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    test();
+    //solve();
+}
+
+/*
+
+*/
