71ab117e8902fd6c364d859b184de2693c9f3f56
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-08-10 16:55:13 +0600

    Create A. Codeforces Checking_849.cpp

---
+++ A. Codeforces Checking_849.cpp
@@ -0,0 +1,45 @@
+//A. Codeforces Checking_849.cpp
+#include<bits/stdc++.h>
+
+  // reverse(s.begin(), s.end());
+using namespace std;
+typedef long long ll;
+//int j=0;
+
+void solve()
+{
+    long long n,i,j,count=0;
+    char ch;
+    cin>>ch;
+    string s="codeforces";
+    int pos = s.find(ch);
+    if(pos==-1)
+        cout<<"NO"<<endl;
+    else
+        cout<<"YES"<<endl;
+}
+void test()
+{
+    long long t;
+    cin>>t;
+
+    while(t--)
+    {
+        //j++;
+        solve();
+    }
+
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    test();
+    //solve();
+}
+/*
+
+*/
