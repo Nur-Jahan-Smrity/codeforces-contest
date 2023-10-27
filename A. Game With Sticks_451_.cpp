63371b8080bd9a2a71d58d91fcf447aa0303a5f1
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-10-27 23:14:01 +0600

    Create A. Game With Sticks_451_.cpp

---
+++ A. Game With Sticks_451_.cpp
@@ -0,0 +1,45 @@
+//A. Game With Sticks_451_.cpp
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+//int j=0;
+//str.find('s')
+void solve()
+{
+
+    ll i,j,n,m;
+    cin>>n>>m;
+
+    ll minn=min(n,m);
+    if(minn%2==0 )
+        cout<<"Malvika"<<endl;
+    else
+        cout<<"Akshat"<<endl;
+
+
+}
+void test()
+{
+    long long t;
+    cin>>t;
+
+    while(t--)
+    {
+        //j++;
+         solve();
+        //cin.ignore();
+
+    }
+
+}
+
+int main()
+{
+
+
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    //test();
+    solve();
+}
+/*
+*/
