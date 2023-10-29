7826d614eb9ba33ab4acfb148c2f15ee4a914a48
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-10-29 08:20:01 +0600

    Create A. Vasya and Socks_460_.cpp

---
+++ A. Vasya and Socks_460_.cpp
@@ -0,0 +1,45 @@
+//A. Vasya and Socks_460_.cpp
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+//int j=0;
+//str.find('s')
+void solve()
+{
+
+    ll i,j,n,m,sum=0,count=0;
+    cin>>n>>m;
+
+    // ...i<=n/m....
+    for(i=1; m*i<=n; i++)
+    {
+        n++;
+    }
+    cout<<n<<endl;
+}
+void test()
+{
+    long long t;
+    cin>>t;
+
+    while(t--)
+    {
+        //j++;
+         solve();
+        //cin.ignore();
+
+    }
+
+}
+
+int main()
+{
+
+
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    //test();
+    solve();
+}
+/*
+
+*/
