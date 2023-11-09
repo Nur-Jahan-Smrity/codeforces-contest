57bccf4a1810c80614ec4c77ed121e949cc59fe8
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-11-09 10:10:02 +0600

    Create A. Filling Diamonds_1339_.cpp

---
+++ A. Filling Diamonds_1339_.cpp
@@ -0,0 +1,38 @@
+//A. Filling Diamonds_1339_.cpp
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve()
+{
+
+    ll i,j,n;
+    cin>>n;
+    cout<<n<<endl;
+
+}
+void test()
+{
+    long long t;
+    cin>>t;
+
+    while(t--)
+    {
+        //j++;
+         solve();
+        //cin.ignore();
+
+    }
+
+}
+
+int main()
+{
+
+
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    test();
+    //solve();
+}
+/*
+
+*/
