dfdb1e9578248e2ea840d29acd94fd3567463321
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-12-06 22:16:31 +0600

    Create A. Boring Apartments_1433_.cpp

---
+++ A. Boring Apartments_1433_.cpp
@@ -0,0 +1,46 @@
+//A. Boring Apartments_1433_.cpp
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
+
+void solve(){
+
+    ll i,j,n;
+   string s;
+   cin>>s;
+   int p=s[0]-48;
+   p=(p-1)*10;
+   n=s.size();
+   p+=(n*(n+1))/2;
+   cout<<p<<endl;
+//1 2 3 4
+//1 3 6 10
+}
+void test()
+{
+    long long t;
+    cin>>t;
+
+    while(t--)
+    {
+        //j++;
+        solve();
+        //cin.ignore();
+
+    }
+
+}
+
+int main()
+{
+
+
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    test();
+    //solve();
+}
+/*
+
+*/
